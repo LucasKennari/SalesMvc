e8b42282e49aa513914687255f141053d37e5852
lucaskennari <lucas.oheisen@gmail.com>
2024-06-04 17:35:06 -0300

    feat: adicionado a dependencia do departmentService. Metodo Create modificado, acrescentando o metodo findAll criado no departmentService

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -7,8 +8,10 @@ namespace SalesWebMvc.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-        public SellersController(SellerService sellerService)
+        private readonly DepartmentService _departmentService;
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
+            _departmentService = departmentService;
             _sellerService = sellerService;
         }
         public IActionResult Index()
@@ -19,8 +22,11 @@ namespace SalesWebMvc.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
+
         [HttpPost]
        [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
