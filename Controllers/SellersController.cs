86dafc092f40373b7ce5355f733dd49070382744
lucaskennari <lucas.oheisen@gmail.com>
2024-06-06 13:59:42 -0300

    feat: implementado metodo Delete

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -5,35 +5,62 @@ using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
-    public class SellersController : Controller
-    {
-        private readonly SellerService _sellerService;
-        private readonly DepartmentService _departmentService;
-        public SellersController(SellerService sellerService, DepartmentService departmentService)
-        {
-            _departmentService = departmentService;
-            _sellerService = sellerService;
-        }
-        public IActionResult Index()
-        {
-            var list = _sellerService.FindAll();
+	public class SellersController : Controller
+	{
+		private readonly SellerService _sellerService;
+		private readonly DepartmentService _departmentService;
+		public SellersController(SellerService sellerService, DepartmentService departmentService)
+		{
+			_departmentService = departmentService;
+			_sellerService = sellerService;
+		}
+		public IActionResult Index()
+		{
+			var list = _sellerService.FindAll();
 
-            return View(list);
-        }
-        public IActionResult Create()
-        {
-            var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments };
-            return View(viewModel);
-        }
+			return View(list);
+		}
+		public IActionResult Create()
+		{
+			var departments = _departmentService.FindAll();
+			var viewModel = new SellerFormViewModel { Departments = departments };
+			return View(viewModel);
+		}
 
-        [HttpPost]
-       [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller)
-        {
-            _sellerService.Insert(seller);
-            return RedirectToAction(nameof(Index));
-        }
-    }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Seller seller)
+		{
+			_sellerService.Insert(seller);
+			return RedirectToAction(nameof(Index));
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var obj = _sellerService.FindById(id.Value);
+
+			if (obj == null)
+			{
+
+				return NotFound();
+			}
+			return View(obj);
+
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(int id)
+		{
+			_sellerService.Remove(id);
+
+			return RedirectToAction(nameof(Index));
+		}
+
+	}
 
 }
