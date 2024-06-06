1a61f4e5f91e6347005b7c6da7be8c69054a8600
lucaskennari <lucas.oheisen@gmail.com>
2024-06-06 13:58:08 -0300

    -

--- Models/SalesRecord.cs
+++ Models/SalesRecord.cs
@@ -18,17 +18,17 @@ namespace SalesWebMvc.Models
 
         }
 
-        public SalesRecord(DateTime date, double amount, SaleStatus status, Seller seller)
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
-
+            Id = id;
             Date = date;
             Amount = amount;
             Status = status;
             Seller = seller;
         }
-        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        public SalesRecord(DateTime date, double amount, SaleStatus status, Seller seller)
         {
-            Id = id;
+
             Date = date;
             Amount = amount;
             Status = status;
