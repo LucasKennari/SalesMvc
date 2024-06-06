a1fcc476c89b12c9cd7e7477a0b32e6e0efad58c
lucaskennari <lucas.oheisen@gmail.com>
2024-06-06 15:34:19 -0300

    feat: adicionado a implementacao de detalhes

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -34,6 +34,25 @@ namespace SalesWebMvc.Controllers
 			_sellerService.Insert(seller);
 			return RedirectToAction(nameof(Index));
 		}
+
+		public IActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+
+			}
+
+			var obj =  _sellerService.FindById(id.Value);
+
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			return View(obj);
+		}
+
 		public IActionResult Delete(int? id)
 		{
 			if (id == null)
